7dd0689c015f5b86a5ed322e1d2bc4af6797b22d
Muskan-Bora <muskanbora2002@gmail.com>
2026-06-04 20:25:08 +0530

    Started  Problem 3 - Python Data Types

---
+++ 01_Python_Fundamentals/prob_03_data_types.py
@@ -0,0 +1,13 @@
+# -------------------------------------------
+
+# Python Data Types
+
+# IMP Note:
+"""
+A variable is a container.
+
+A data type tells Python:
+
+"What kind of data is stored inside the container?"
+"""
+
