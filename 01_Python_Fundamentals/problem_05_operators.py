a6f1b8e0c6583f6f87b29b25803c8ddd1d84c9b5
Muskan-Bora <muskanbora2002@gmail.com>
2026-06-06 20:49:55 +0530

    Python Operators started with Intro

---
+++ 01_Python_Fundamentals/problem_05_operators.py
@@ -0,0 +1,72 @@
+# ------------------------------------------------------- #
+
+# Python Operators
+
+# ------------------------------------------------------- #
+
+"""
+Python Operators
+
+Definition:
+Operators are special symbols used to perform operations on variables and values.
+
+Operators help us:
+
+1. Perform mathematical calculations.
+2. Compare values.
+3. Combine conditions.
+4. Assign values to variables.
+5. Check membership and identity.
+
+Common Types of Operators in Python:
+
+1. Arithmetic Operators
+   +   Addition
+   -   Subtraction
+   *   Multiplication
+   /   Division
+   %   Modulus
+   **  Exponent
+   //  Floor Division
+
+2. Comparison Operators
+   ==  Equal To
+   !=  Not Equal To
+   >   Greater Than
+   <   Less Than
+   >=  Greater Than or Equal To
+   <=  Less Than or Equal To
+
+3. Assignment Operators
+   =
+   +=
+   -=
+   *=
+   /=
+   %=
+
+4. Logical Operators
+   and
+   or
+   not
+
+5. Membership Operators
+   in
+   not in
+
+6. Identity Operators
+   is
+   is not
+
+Why Are Operators Important?
+
+Operators are used everywhere in Python:
+- Calculations
+- Conditions
+- Loops
+- Functions
+- APIs
+- Django Projects
+- Data Analysis
+- AI & Machine Learning
+"""
