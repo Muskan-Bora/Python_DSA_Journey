8120e50967c41f6cc9e5fea00b9c9b0f5d12f4ba
Muskan-Bora <muskanbora2002@gmail.com>
2026-06-08 13:16:20 +0530

    Completed Python Operators

--- 01_Python_Fundamentals/problem_05_operators.py
+++ 01_Python_Fundamentals/problem_05_operators.py
@@ -92,16 +92,16 @@ Exponent
 Floor Division
 """
 
-# num1 = 20
-# num2 = 5
+num1 = 20
+num2 = 5
 
-# print("Addition: ", num1, "+", num2, "=", num1 + num2)   
-# print("Subtraction: ", num1, "-", num2, "=", num1 - num2)
-# print("Multiplication: ", num1, "*", num2, "=", num1 * num2)
-# print("Division: ", num1, "/", num2, "=", num1 / num2)
-# print("Modulus: ", num1, "%", num2, "=", num1 % num2)
-# print("Exponent: ", num1, "**", num2, "=", num1 ** num2)
-# print("Floor Division: ", num1, "//", num2, "=", num1 // num2)
+print("Addition: ", num1, "+", num2, "=", num1 + num2)   
+print("Subtraction: ", num1, "-", num2, "=", num1 - num2)
+print("Multiplication: ", num1, "*", num2, "=", num1 * num2)
+print("Division: ", num1, "/", num2, "=", num1 / num2)
+print("Modulus: ", num1, "%", num2, "=", num1 % num2)
+print("Exponent: ", num1, "**", num2, "=", num1 ** num2)
+print("Floor Division: ", num1, "//", num2, "=", num1 // num2)
 
 """
 Output:
@@ -129,13 +129,13 @@ Product
 Division
 """
 
-# num_1 = int(input("Enter first number: "))
-# num_2 = int(input("Enter second number: "))
+num_1 = int(input("Enter first number: "))
+num_2 = int(input("Enter second number: "))
 
-# print(f"Sum: {num_1 + num_2}")
-# print(f"Difference: {num_1 - num_2}")
-# print(f"Product: {num_1 * num_2}")
-# print(f"Division: {num_1 / num_2}")
+print(f"Sum: {num_1 + num_2}")
+print(f"Difference: {num_1 - num_2}")
+print(f"Product: {num_1 * num_2}")
+print(f"Division: {num_1 / num_2}")
 
 """
 Output:
@@ -309,3 +309,169 @@ Enter your age: 16
 Do you have the ticket (True/Flase): True
 Can Enter: False
 """
+
+# ------------------------------------------- #
+
+"""
+Problem 8: Membership Operators [in, not in]
+
+Task
+Create:
+programming_languages = ["Python", "Java", "JavaScript"]
+"""
+
+programming_languages = ["Python", "Java", "JavaScript"]
+
+print("Python" in programming_languages)          # Output --> True
+print("C++" in programming_languages)             # Output --> False
+print("Ruby" not in programming_languages)        # Output --> True
+
+# ------------------------------------------- #
+
+"""
+Problem 9: Identity Operators [is , is not]
+
+Task
+Create:
+a = 10
+b = 10
+"""
+
+a = 10
+b = 10
+
+print(a is b)                # Output --> True
+print(a is not b)            # Output --> False
+
+# ------------------------------------------ #
+
+"""
+Problem 10: Operator Precedence [PEMDAS/BODMAS]
+
+Task
+Predict first, then execute:
+
+result = 10 + 5 * 2
+"""
+
+result = 10 + 5 * 2
+print(result) 
+
+"""
+Output: 20
+Explanation while Predicting
+Used BODMAS:
+result = 10 + 5 * 2
+result = 10 + 10 
+result = 20 // As per the BODMAS 1st Multiplication applied and than addition
+"""
+
+# --------
+# Another Example:
+
+result = (10 + 5) * 2
+print(result)
+
+"""
+Output: 30
+Explanation while Predicting:
+In this the bracket should get solved 1st so 10 + 5 = 15 than 15 * 2 = 30
+So, here result = 30
+"""
+
+# --------------------------------------------- #
+
+# BONUS CAHALLENGE:
+
+"""
+Bonus Challenge 1: Mini Salary Increment Calculator
+
+Take:
+Current Salary
+Increment Percentage
+
+Calculate new salary.
+"""
+
+print("Mini Salary Increment Calculator")
+
+current_salary_per_year = int(input("Enter your Current Salary of this year: "))
+increment_percentage_per_year = int(input("Enter your increment percentage for the next year: "))
+
+new_salary_increment_per_year = current_salary_per_year * increment_percentage_per_year / 100
+new_salary_per_year = new_salary_increment_per_year + current_salary_per_year
+
+new_salary_increment_per_month = new_salary_increment_per_year / 12
+new_salary_per_month = new_salary_per_year / 12
+
+print(f"As per your increment percentage the increment amount is {new_salary_increment_per_year} for next year")
+print(f"So, your new salary is {new_salary_per_year} for the next year..")
+
+print(f"As per your increment percentage the increment amount for per month is {new_salary_increment_per_month}")
+print(f"So, your new salary for the per month is {new_salary_per_month}")
+
+"""
+Output:
+Mini Salary Increment Calculator
+Enter your Current Salary of this year: 300000
+Enter your increment percentage for the next year: 20
+As per your increment percentage the increment amount is 60000.0 for next year
+So, your new salary is 360000.0 for the next year..
+As per your increment percentage the increment amount for per month is 5000.0
+So, your new salary for the per month is 30000.0
+"""
+
+# ------------------------------------------- #
+
+"""
+Bonus Challenge 2: Login Verification
+
+Create:
+stored_username = "admin"
+stored_password = "python123"
+
+Take user input.
+
+Check:
+username == stored_username
+and
+password == stored_password
+
+Print:
+Login Successful: True
+
+or
+
+Login Successful: False
+"""
+
+stored_username = "admin"
+stored_password = "python123"
+
+print("Login Verification")
+
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+login_successful = username == stored_username and password == stored_password
+print(f"Login Successful: {login_successful}")
+
+"""
+Output:
+Login Verification
+Enter your username: admin
+Enter your password: Python123
+Login Successful: False
+
+Login Verification
+Enter your username: Admin
+Enter your password: python123
+Login Successful: False
+
+Login Verification
+Enter your username: admin
+Enter your password: python123
+Login Successful: True
+"""
+
+# ------------------------------------------------------------------- #
