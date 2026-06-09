5764f5b6c57946c3b1e43064bb6e71264f2adbc2
Muskan-Bora <muskanbora2002@gmail.com>
2026-06-09 20:39:00 +0530

    Python Type Casting Started

---
+++ 01_Python_Fundamentals/prob_06_type_casting.py
@@ -0,0 +1,73 @@
+# ------------------------------------------------------- #
+
+# Python Type Casting
+
+# ------------------------------------------------------- #
+
+"""
+Python Type Casting
+
+Definition:
+Type Casting is the process of converting one data type
+into another data type.
+
+Why Do We Need Type Casting?
+
+Sometimes data is available in one type but we need it in another type to perform calculations, comparisons,
+or other operations.
+
+Example:
+
+age = "24"
+
+The value above is a string.
+
+To perform mathematical operations, we must convert it:
+
+age = int(age)
+
+Now age becomes an integer.
+
+Common Type Casting Functions:
+
+1. int()
+   Converts data into Integer
+
+   Example:
+   int("100") → 100
+
+2. float()
+   Converts data into Float
+
+   Example:
+   float("99.5") → 99.5
+
+3. str()
+   Converts data into String
+
+   Example:
+   str(100) → "100"
+
+4. bool()
+   Converts data into Boolean
+
+   Example:
+   bool(1) → True
+   bool(0) → False
+
+Type Casting is widely used in:
+- User Input
+- Calculations
+- Conditions
+- Loops
+- APIs
+- Django Projects
+- Data Analysis
+- AI & Machine Learning
+
+Important Note:
+
+input() always returns a string.
+
+Therefore type casting is often required when taking numeric input from users.
+"""
