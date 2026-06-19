14a7c01170389728439eda35012dcb8849661693
Muskan-Bora <muskanbora2002@gmail.com>
2026-06-19 21:07:44 +0530

    Python - Solved Problem 6 of While Loop

--- 02_Loops/prob_01_intro_to_loops.py
+++ 02_Loops/prob_01_intro_to_loops.py
@@ -330,3 +330,36 @@ Blast Off!
 """
 
 # ----------------------------------
+
+"""
+Problem 6 — Multiplication Table of 3
+
+Using while loop, print:
+
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+...
+3 x 10 = 30
+"""
+
+number = 1
+mul = 3
+
+while number < 11:
+    print(mul, "x", number, "=", mul * number)
+    number += 1
+
+"""
+Output:
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
+"""
