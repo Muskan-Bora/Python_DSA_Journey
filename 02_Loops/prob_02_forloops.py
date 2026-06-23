f950146b719a52ef1eefde3cb3217105f3a0009a
Muskan-Bora <muskanbora2002@gmail.com>
2026-06-23 21:05:21 +0530

    Python for loop intro

---
+++ 02_Loops/prob_02_forloops.py
@@ -0,0 +1,27 @@
+# =========================================
+# FOR LOOP IN PYTHON
+# =========================================
+
+"""
+1) What is a For Loop?
+
+A for loop is used to repeat a block of code for each item in a sequence.
+
+Sequence can be:
+
+String
+List
+Tuple
+Range
+Set
+Dictionary (later)
+
+Example:
+
+for x in sequence:
+    # code
+
+Meaning:
+
+“Take each value from sequence one by one and run the code.”
+"""
