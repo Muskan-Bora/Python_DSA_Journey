daf23ade5b17bb17c32e26dc67ccdb921bd1d120
Muskan-Bora <muskanbora2002@gmail.com>
2026-07-28 19:33:31 +0530

    python pattern problem 1 solved

---
+++ 02_Loops/prob_03_patterns.py
@@ -0,0 +1,81 @@
+# ===================================================
+
+# Python Patterns
+
+"""
+Pattern 1 — Half Pyramid of Stars
+"""
+for i in range(1, 6):
+    print("*" * i)    # Shortcut pythonic way
+
+
+for i in range(1, 6):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+"""
+Output:
+*
+**
+***
+****
+*****
+
+Explanation:
+i = Row Number
+
+The number of stars printed = i
+
+So the execution becomes:
+
+i = 1
+↓
+Print "*" x 1
+↓
+
+*
+
+-------------------
+
+i = 2
+↓
+Print "*" x 2
+↓
+
+**
+
+-------------------
+
+i = 3
+↓
+Print "*" x 3
+↓
+
+***
+
+-------------------
+
+i = 4
+↓
+
+****
+
+-------------------
+
+i = 5
+↓
+
+*****
+
+Notice here i is doing two jobs:
+
+It tells us which row we're on.
+It also tells us how many stars to print.
+
+That's why:
+
+print("*" * i)
+
+works so beautifully.
+"""
