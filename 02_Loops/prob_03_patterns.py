19cec721faf2378c91a9f179eef88477e42b23eb
Muskan-Bora <muskanbora2002@gmail.com>
2026-07-29 19:34:50 +0530

    python pattern problem 3 and 4 solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -102,3 +102,46 @@ Output:
 """
 
 # =======================================
+
+"""
+Pattern 3 — Half Pyramid of Numbers
+"""
+
+print()
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
+"""
+Output:
+1
+12
+123
+1234
+12345
+"""
+
+# ===========================================
+
+"""
+Pattern 4 — Inverted Half Pyramid of Numbers
+"""
+print()
+
+for i in range(6, 1, -1):
+    for j in range(1, i): 
+        print(j, end="") 
+    print()
+
+"""
+Output:
+12345
+1234
+123
+12
+1
+"""
+
+# ===============================================
