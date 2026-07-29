33609647bf3eaa88eaafeea23862e956724745ef
Muskan-Bora <muskanbora2002@gmail.com>
2026-07-29 19:36:09 +0530

    python pattern problem 5 solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -145,3 +145,25 @@ Output:
 """
 
 # ===============================================
+
+"""
+Pattern 5 — Repeated Row Numbers
+"""
+
+print()
+
+for i in range(1, 6):
+    for j in range(i):
+        print(i, end="")
+    print()
+
+"""
+Output:
+1
+22
+333
+4444
+55555
+"""
+
+# =========================
