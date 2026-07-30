92c537103dbe2079b1d1cc06e569cd3b58e03458
Muskan-Bora <muskanbora2002@gmail.com>
2026-07-30 13:58:28 +0530

    python pattern problem 9 solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -236,3 +236,45 @@ for i in range(1, 6):    # Outer Loop
 """
 
 # ===================================
+
+"""
+Pattern 9 — Full Pyramid of Stars
+"""
+# Method 1
+print()
+for i in range(1, 10, 2):
+    for k in range(9 - i):
+        print(" ", end="")
+    for j in range(i):
+        print("*", end=" ")
+    print()
+
+# Method 2 common
+
+print()
+
+for i in range(1, 6):
+    for k in range(5-i):
+        print(" ", end="")
+    for j in range(i * 2 - 1) :
+        print("*", end="")
+    print()
+
+"""
+Method 1: 
+
+        * 
+      * * * 
+    * * * * * 
+  * * * * * * * 
+* * * * * * * * * 
+
+Method 2:
+    *
+   ***
+  *****
+ *******
+*********
+"""
+
+# =================================
