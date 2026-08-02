792a590fd93f90c36db93f8454b6175d1b48a93b
Muskan-Bora <muskanbora2002@gmail.com>
2026-08-02 22:33:34 +0530

    python pattern revision prob 5 solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -541,3 +541,25 @@ Output:
 """
 
 # ===================================================
+
+"""
+Pattern 5 — Left-Aligned Number Triangle
+"""
+
+print()
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
+"""
+Output:
+1
+12
+123
+1234
+12345
+"""
+
+# ==================================
