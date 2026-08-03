561e8c674785dbf2a8eaf99c1180987371c82824
Muskan-Bora <muskanbora2002@gmail.com>
2026-08-03 13:42:31 +0530

    python patterns - new pattern 3 solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -686,3 +686,35 @@ Output:
 """
 
 # ===================================
+
+"""
+New Pattern 3 — Palindrome Number Triangle
+Problem Statement
+
+Write a Python program using nested for loops to print:
+
+1
+121
+12321
+1234321
+123454321
+"""
+
+print()
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end="")
+    for l in range(i-1, 0, -1):
+        print(l, end="")
+    print()
+
+"""
+Output:
+1
+121
+12321
+1234321
+123454321
+"""
+
+# ========================================
