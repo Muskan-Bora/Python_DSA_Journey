c07bdfdc24545ce6cf5a4197b706baa91b088965
Muskan-Bora <muskanbora2002@gmail.com>
2026-08-08 23:58:30 +0530

    Python pattern revsion prob solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -995,3 +995,26 @@ Output:
 """
 
 # ====================================
+
+"""
+Revision — Full Pyramid of Stars
+"""
+
+print()
+
+for i in range(1, 6):
+    for k in range(5-i):
+        print(" ", end="")
+    for j in range(i * 2 - 1) :
+        print("*", end="")
+    print()
+
+"""
+    *
+   ***
+  *****
+ *******
+*********
+"""
+
+# =================================
