066c0092e1093a51512e24652151db79401ad369
Muskan-Bora <muskanbora2002@gmail.com>
2026-08-08 23:59:48 +0530

    Python pattern revsion prob solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -1018,3 +1018,45 @@ for i in range(1, 6):
 """
 
 # =================================
+
+"""
+Revision — Half Diamond Star Pattern
+Problem Statement
+
+Write a Python program using nested for loops to print the following pattern:
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+"""
+
+print()
+
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
+
+for i in range(4, 0, -1):
+    for j in range(i):
+        print("*", end="")
+    print()
+
+"""
+Output:
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+"""
