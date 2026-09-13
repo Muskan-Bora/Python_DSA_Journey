7ee71ffee42c9c58944f8cc87ae4705f59da8594
Muskan-Bora <muskanbora2002@gmail.com>
2026-09-13 22:47:01 +0530

    Python 1 revision star pattern solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -2701,3 +2701,44 @@ Output:
 """
 
 # ========================
+
+"""
+⭐ Star Pattern Revision
+
+Pattern
+
+Print:
+
+    *
+   ***
+  *****
+ *******
+*********
+Rules
+Use nested for loops.
+Don't hardcode the stars.
+Use i to control the rows.
+You already know the required space + star relationship from your previous pyramid practice.
+"""
+
+print()
+
+for i in range(1, 6):
+    for k in range(5 - i):
+        print(" ", end="")
+    for j in range(1, i + 1):
+        print("*", end="")
+    for s in range(1, i * 1):
+        print("*", end="")
+    print()
+
+"""
+Output:
+    *
+   ***
+  *****
+ *******
+*********
+"""
+
+# =======================================
