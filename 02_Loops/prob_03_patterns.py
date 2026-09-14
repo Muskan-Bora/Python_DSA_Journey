871badc5b830dba59906b6f4f5b6c39e15ecc90e
Muskan-Bora <muskanbora2002@gmail.com>
2026-09-14 23:51:39 +0530

    Python 1 more revision star pattern solved

--- 02_Loops/prob_03_patterns.py
+++ 02_Loops/prob_03_patterns.py
@@ -2830,3 +2830,37 @@ Output:
 **
 *
 """
+
+# ======================================
+
+"""
+⭐ Problem 4 — Right-Aligned Star Triangle
+
+Print:
+
+    *
+   **
+  ***
+ ****
+*****
+"""
+
+print()
+
+for i in range(1, 6):
+    for k in range(5 - i):
+        print(" ", end="")
+    for j in range(1, i + 1):
+        print("*", end="")
+    print()
+
+"""
+Output:
+    *
+   **
+  ***
+ ****
+*****
+"""
+
+# ========================================================
