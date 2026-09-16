167569d6b113a20119dcf78839cecd36031a6fc0
Muskan-Bora <muskanbora2002@gmail.com>
2026-09-16 20:52:55 +0530

    Python Strings 2 Problems solved

---
+++ 04_Strings/prob_01_string_indexing.py
@@ -0,0 +1,88 @@
+## =========================================== ##
+
+"""
+String Indexing Problem
+"""
+
+# ===================================
+
+"""
+🧩 String Problem 1 — Basic Indexing
+
+Write a Python program that:
+
+Creates a string variable:
+
+text = "PYTHON"
+Print the first character of the string.
+Print the third character of the string.
+Print the last character of the string.
+Expected output
+P
+T
+N
+
+💡 Rules:
+
+Don't use a loop.
+Don't use len() yet.
+Use string indexing.
+"""
+
+text = "PYTHON"
+
+print(text[0])
+print(text[2])
+print(text[5])
+
+"""
+Output:
+P
+T
+N
+"""
+
+# ===========================================
+
+"""
+🧩 String Problem 2 — Negative Indexing
+
+Now let's test the negative indexing you noted.
+
+Given:
+
+text = "PYTHON"
+
+Write a program that prints:
+
+The last character
+The second-last character
+The third-last character
+Expected output
+N
+O
+H
+💡 Rules
+Don't use a loop.
+Don't use len().
+Use negative indexing.
+
+Take your time and write it yourself. 🐍💪
+"""
+
+print()
+
+text = "PYTHON"
+
+print(text[-1])
+print(text[-2])
+print(text[-3])
+
+"""
+Output:
+N
+O
+H
+"""
+
+# ========================================
