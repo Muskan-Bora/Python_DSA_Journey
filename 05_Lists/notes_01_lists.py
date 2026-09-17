92b65d8a7ff17639638e438e6ccd31a37c7ccb22
Muskan-Bora <muskanbora2002@gmail.com>
2026-09-17 20:46:10 +0530

    Python Lists Notes Started

---
+++ 05_Lists/notes_01_lists.py
@@ -0,0 +1,42 @@
+# ========================================================
+
+# Lists 
+
+"""
+🐍 Python Lists — Notes
+
+1. What is a List?
+
+A list is an ordered collection of multiple values stored in a single variable.
+
+fruits = ["apple", "banana", "mango"]
+
+A list can contain different data types:
+
+data = ["Muskan", 24, 5.6, True]
+Key points
+Ordered ✅
+Indexed ✅
+Mutable ✅
+Allows duplicate values ✅
+Can store different data types ✅
+
+# -------------------------------------------
+2. Creating a List
+
+Use square brackets [].
+
+numbers = [10, 20, 30, 40]
+
+names = ["Muskan", "Rahul", "Priya"]
+
+empty_list = []
+
+Check type:
+
+print(type(numbers))
+
+Output:
+
+<class 'list'>
+"""
